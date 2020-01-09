ef0d771e2f0609b4c24cd7267e5eab3c54e9f538
Pieter Noordhuis <pietern@fb.com>
2020-01-09 11:32:37 +0100

    Implement and test shm transport context class

---
+++ tensorpipe/transport/shm/context.cc
@@ -0,0 +1,30 @@
+#include <tensorpipe/transport/shm/context.h>
+
+#include <tensorpipe/transport/shm/connection.h>
+#include <tensorpipe/transport/shm/listener.h>
+
+namespace tensorpipe {
+namespace transport {
+namespace shm {
+
+Context::Context() : loop_(std::make_shared<Loop>()) {}
+
+Context::~Context() {}
+
+std::shared_ptr<transport::Connection> Context::connect(address_t addr) {
+  auto sockaddr = Sockaddr::createAbstractUnixAddr(addr);
+  auto socket = Socket::createForFamily(AF_UNIX);
+  socket->connect(sockaddr);
+  return std::make_shared<Connection>(loop_, std::move(socket));
+}
+
+std::shared_ptr<transport::Listener> Context::listen(
+    address_t addr,
+    transport::Listener::connection_callback_fn fn) {
+  auto sockaddr = Sockaddr::createAbstractUnixAddr(addr);
+  return std::make_shared<Listener>(loop_, sockaddr, std::move(fn));
+}
+
+} // namespace shm
+} // namespace transport
+} // namespace tensorpipe
