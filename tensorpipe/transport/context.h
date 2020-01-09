700782a544d570b52de9dd6297ea2e4d0c0bbae8
Pieter Noordhuis <pietern@fb.com>
2020-01-09 11:31:34 +0100

    Define the listener callback function in base class

--- tensorpipe/transport/context.h
+++ tensorpipe/transport/context.h
@@ -3,11 +3,11 @@
 #include <memory>
 #include <string>
 
+#include <tensorpipe/transport/connection.h>
+#include <tensorpipe/transport/listener.h>
+
 namespace tensorpipe {
 namespace transport {
-
-class Connection;
-class Listener;
 
 class Context {
  public:
@@ -17,7 +17,9 @@ class Context {
 
   virtual std::shared_ptr<Connection> connect(address_t addr) = 0;
 
-  virtual std::shared_ptr<Listener> listen(address_t addr) = 0;
+  virtual std::shared_ptr<Listener> listen(
+      address_t addr,
+      Listener::connection_callback_fn fn) = 0;
 };
 
 } // namespace transport
