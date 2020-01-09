a1ca22369dd9446a3463e2ca82921039e264eb4a
Pieter Noordhuis <pietern@fb.com>
2020-01-09 11:32:37 +0100

    Implement and test shm transport context class

---
+++ tensorpipe/test/transport/shm/context_test.cc
@@ -0,0 +1,34 @@
+#include <tensorpipe/common/defs.h>
+#include <tensorpipe/transport/shm/context.h>
+
+#include <gtest/gtest.h>
+
+using namespace tensorpipe::transport;
+
+TEST(Context, Basics) {
+  auto context = std::make_shared<shm::Context>();
+  auto addr = "foobar";
+
+  std::mutex mutex;
+  std::condition_variable cv;
+  std::vector<std::shared_ptr<Connection>> connections;
+
+  // Listener runs callback for every new connection.
+  auto listener =
+      context->listen(addr, [&](std::shared_ptr<Connection> connection) {
+        std::lock_guard<std::mutex> lock(mutex);
+        connections.push_back(std::move(connection));
+        cv.notify_one();
+      });
+
+  // Connect to listener.
+  auto conn = context->connect(addr);
+
+  // Wait for new connection
+  {
+    std::unique_lock<std::mutex> lock(mutex);
+    while (connections.empty()) {
+      cv.wait(lock);
+    }
+  }
+}
---
+++ tensorpipe/transport/shm/context.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <tensorpipe/transport/context.h>
+#include <tensorpipe/transport/shm/loop.h>
+
+namespace tensorpipe {
+namespace transport {
+namespace shm {
+
+class Context final : public transport::Context {
+ public:
+  explicit Context();
+
+  ~Context() override;
+
+  std::shared_ptr<transport::Connection> connect(address_t addr) override;
+
+  std::shared_ptr<transport::Listener> listen(
+      address_t addr,
+      transport::Listener::connection_callback_fn fn) override;
+
+ private:
+  std::shared_ptr<Loop> loop_;
+};
+
+} // namespace shm
+} // namespace transport
+} // namespace tensorpipe
