00fd326111400035413d7b18236beabe63cdad24
Pieter Noordhuis <pietern@fb.com>
2020-01-13 10:11:32 +0100

    Fix formatting

--- tensorpipe/transport/listener.h
+++ tensorpipe/transport/listener.h
@@ -12,8 +12,7 @@ class Listener {
  public:
   virtual ~Listener() = default;
 
-  using accept_callback_fn =
-      std::function<void(std::shared_ptr<Connection>)>;
+  using accept_callback_fn = std::function<void(std::shared_ptr<Connection>)>;
 
   virtual void accept(accept_callback_fn fn) = 0;
 };
