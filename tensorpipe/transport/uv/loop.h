0510682d44e9e57d0fae245f8db50f1b1603a574
Luca Wehrstedt <lcw@fb.com>
2020-02-04 06:50:49 -0800

    Undo the restriction of not calling run from UV loop
    Summary:
    This basically undoes part of D19690868 because, well, we needed that.
    
    For example, in the accept callback (which is called from the UV loop) we might want to get the listener's address in order to write it to the connection.
    
    Reviewed By: pietern
    
    Differential Revision: D19720036
    
    fbshipit-source-id: 644f88f4f6074a7fcbf16266de182d0a9aaf27f8

--- tensorpipe/transport/uv/loop.h
+++ tensorpipe/transport/uv/loop.h
@@ -43,23 +43,29 @@ class Loop final : public std::enable_shared_from_this<Loop> {
   // Prefer using defer over run, when you don't need to wait for the result.
   template <typename F>
   void run(F&& fn) {
-    TP_THROW_ASSERT_IF(std::this_thread::get_id() == thread_.get_id())
-        << "Loop::run was called from the event loop thread itself (e.g., from "
-        << "a callback), which would have caused a deadlock because the given "
-        << "callable can only be run when the loop is allowed to proceed";
-    // Must use a copyable wrapper around std::promise because
-    // we use it from a std::function which must be copyable.
-    auto promise = std::make_shared<std::promise<void>>();
-    auto future = promise->get_future();
-    defer([promise, fn{std::forward<F>(fn)}]() {
-      try {
-        fn();
-        promise->set_value();
-      } catch (...) {
-        promise->set_exception(std::current_exception());
-      }
-    });
-    future.get();
+    // When called from the event loop thread itself (e.g., from a callback),
+    // deferring would cause a deadlock because the given callable can only be
+    // run when the loop is allowed to proceed. On the other hand, it means it
+    // is thread-safe to run it immediately. The danger here however is that it
+    // can lead to an inconsistent order between operations run from the event
+    // loop, from outside of it, and deferred.
+    if (std::this_thread::get_id() == thread_.get_id()) {
+      fn();
+    } else {
+      // Must use a copyable wrapper around std::promise because
+      // we use it from a std::function which must be copyable.
+      auto promise = std::make_shared<std::promise<void>>();
+      auto future = promise->get_future();
+      defer([promise, fn{std::forward<F>(fn)}]() {
+        try {
+          fn();
+          promise->set_value();
+        } catch (...) {
+          promise->set_exception(std::current_exception());
+        }
+      });
+      future.get();
+    }
   }
 
   // Use this for callbacks that need to run immediately (cannot be deferred)
