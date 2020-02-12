ea318be2da012eff2bad82046188c3dcedc702fc
Luca Wehrstedt <lcw@fb.com>
2020-02-12 05:34:14 -0800

    Add Python bindings
    Summary: Pull Request resolved: https://github.com/pytorch/tensorpipe/pull/65
    
    Reviewed By: pietern
    
    Differential Revision: D19808500
    
    fbshipit-source-id: 6753f6a57ba969ba8a931f93f7cf20e4c5c16299

---
+++ setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+import os
+import subprocess
+import sys
+from pathlib import Path
+
+from setuptools import Extension, setup
+from setuptools.command.build_ext import build_ext
+
+
+class CMakeBuild(build_ext):
+    def run(self):
+        for ext in self.extensions:
+            self.build_extension(ext)
+
+    def build_extension(self, ext):
+        if not os.path.exists(self.build_temp):
+            os.makedirs(self.build_temp)
+
+        source_path = Path(__file__).parent.resolve()
+        output_path = Path(self.get_ext_fullpath(ext.name)).parent.resolve()
+        build_type = "Debug" if self.debug else "Release"
+
+        cmake_cmd = [
+            "cmake",
+            f"{source_path}",
+            f"-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={output_path}",
+            f"-DPYTHON_EXECUTABLE={sys.executable}",
+            f"-DCMAKE_BUILD_TYPE={build_type}",
+            "-DCMAKE_C_COMPILER=clang-6.0",
+            "-DCMAKE_CXX_COMPILER=clang++-6.0",
+            "-DBUILD_SHARED_LIBS=true",
+            "-DBUILD_PYTHON_MODULE=true",
+        ]
+        make_cmd = ["make"]
+
+        subprocess.check_call(cmake_cmd, cwd=self.build_temp)
+        subprocess.check_call(make_cmd, cwd=self.build_temp)
+
+
+setup(
+    name="tensorpipe",
+    version="0.0.0",
+    author="Facebook AI Research",
+    ext_modules=[Extension("pytensorpipe", sources=[])],
+    cmdclass={"build_ext": CMakeBuild},
+    zip_safe=False,
+)
