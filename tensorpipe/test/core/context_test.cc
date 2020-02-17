6d0c9dbd7cf1834930f23a539d3645164952c800
Luca Wehrstedt <lcw@fb.com>
2020-02-17 07:46:53 -0800

    Add a ping-pong test for the pipe
    Summary:
    Add two tests:
    - one that does the same thing our current test was doing, but serially (the main thread waits for each callback to fire before running the next operation, as opposed to the current test where the next operation is run from within the callback).
    - one that has the server start sending stuff, with the client answering, and does this twice.
    
    Reviewed By: pietern
    
    Differential Revision: D19936115
    
    fbshipit-source-id: 5fc3df323faadf8e2910f91014fe329c8552cb12

--- tensorpipe/test/core/context_test.cc
+++ tensorpipe/test/core/context_test.cc
@@ -14,9 +14,11 @@
 #include <tensorpipe/transport/shm/context.h>
 #include <tensorpipe/transport/uv/context.h>
 
-#include <condition_variable>
 #include <cstring>
-#include <mutex>
+#include <exception>
+#include <future>
+#include <memory>
+#include <string>
 
 #include <gtest/gtest.h>
 
@@ -28,39 +30,149 @@ std::unique_ptr<uint8_t[]> copyStringToBuffer(const std::string& s) {
   return b;
 }
 
-bool compareStringToBuffer(
-    const std::string& s,
-    const std::unique_ptr<uint8_t[]>& ptr,
-    size_t len) {
-  return ptr && len == s.size() &&
-      std::memcmp(ptr.get(), s.data(), s.size()) == 0;
+::testing::AssertionResult buffersAreEqual(
+    const std::unique_ptr<uint8_t[]>& ptr1,
+    const size_t len1,
+    const std::unique_ptr<uint8_t[]>& ptr2,
+    const size_t len2) {
+  if (ptr1 == nullptr && ptr2 == nullptr) {
+    if (len1 == 0 && len2 == 0) {
+      return ::testing::AssertionSuccess();
+    }
+    if (len1 != 0) {
+      return ::testing::AssertionFailure()
+          << "first pointer is null but length isn't 0";
+    }
+    if (len1 != 0) {
+      return ::testing::AssertionFailure()
+          << "second pointer is null but length isn't 0";
+    }
+  }
+  if (ptr1 == nullptr) {
+    return ::testing::AssertionFailure()
+        << "first pointer is null but second one isn't";
+  }
+  if (ptr2 == nullptr) {
+    return ::testing::AssertionFailure()
+        << "second pointer is null but first one isn't";
+  }
+  if (len1 != len2) {
+    return ::testing::AssertionFailure()
+        << "first length is " << len1 << " but second one is " << len2;
+  }
+  if (std::memcmp(ptr1.get(), ptr2.get(), len1) != 0) {
+    return ::testing::AssertionFailure() << "buffer contents aren't equal";
+  }
+  return ::testing::AssertionSuccess();
 }
 
-class Defer {
- public:
-  Defer(std::function<void()> f) : f_(std::move(f)){};
-  ~Defer() {
-    f_();
-  };
+::testing::AssertionResult messagesAreEqual(
+    const Message& m1,
+    const Message& m2) {
+  EXPECT_TRUE(buffersAreEqual(m1.data, m1.length, m2.data, m2.length));
+  if (m1.tensors.size() != m2.tensors.size()) {
+    return ::testing::AssertionFailure()
+        << "first message has " << m1.tensors.size() << " but second has "
+        << m2.tensors.size();
+  }
+  for (size_t idx = 0; idx < m1.tensors.size(); idx++) {
+    EXPECT_TRUE(buffersAreEqual(
+        m1.tensors[idx].data,
+        m1.tensors[idx].length,
+        m2.tensors[idx].data,
+        m2.tensors[idx].length));
+  }
+  return ::testing::AssertionSuccess();
+}
 
- private:
-  std::function<void()> f_;
-};
-
-TEST(Context, Simple) {
-  Message sentMessage;
+Message makeMessage() {
+  Message message;
   std::string messageData = "I'm a message";
   std::string tensorData = "And I'm a tensor";
-  sentMessage.data = copyStringToBuffer(messageData);
-  sentMessage.length = messageData.length();
-  Message::Tensor sentTensor;
-  sentTensor.data = copyStringToBuffer(tensorData);
-  sentTensor.length = tensorData.length();
-  sentMessage.tensors.push_back(std::move(sentTensor));
+  message.data = copyStringToBuffer(messageData);
+  message.length = messageData.length();
+  Message::Tensor tensor;
+  tensor.data = copyStringToBuffer(tensorData);
+  tensor.length = tensorData.length();
+  message.tensors.push_back(std::move(tensor));
+  return message;
+}
 
-  bool done = false;
-  std::mutex mutex;
-  std::condition_variable cv;
+TEST(Context, ClientPingSerial) {
+  auto context = Context::create();
+
+  context->registerTransport(
+      0, "uv", std::make_shared<transport::uv::Context>());
+  context->registerTransport(
+      -1, "shm", std::make_shared<transport::shm::Context>());
+  context->registerChannelFactory(
+      0, "basic", std::make_shared<channel::basic::BasicChannelFactory>());
+
+  auto listener = Listener::create(context, {"shm://foobar", "uv://127.0.0.1"});
+
+  auto clientPipe = Pipe::create(context, listener->url("uv"));
+
+  std::promise<std::shared_ptr<Pipe>> serverPipePromise;
+  listener->accept([&](const Error& error, std::shared_ptr<Pipe> pipe) {
+    if (error) {
+      serverPipePromise.set_exception(
+          std::make_exception_ptr(std::runtime_error(error.what())));
+    } else {
+      serverPipePromise.set_value(std::move(pipe));
+    }
+  });
+  std::shared_ptr<Pipe> serverPipe = serverPipePromise.get_future().get();
+
+  std::promise<Message> writtenMessagePromise;
+  clientPipe->write(makeMessage(), [&](const Error& error, Message&& message) {
+    if (error) {
+      writtenMessagePromise.set_exception(
+          std::make_exception_ptr(std::runtime_error(error.what())));
+    } else {
+      writtenMessagePromise.set_value(std::move(message));
+    }
+  });
+
+  std::promise<Message> readDescriptorPromise;
+  serverPipe->readDescriptor([&](const Error& error, Message&& message) {
+    if (error) {
+      readDescriptorPromise.set_exception(
+          std::make_exception_ptr(std::runtime_error(error.what())));
+    } else {
+      readDescriptorPromise.set_value(std::move(message));
+    }
+  });
+
+  Message message(readDescriptorPromise.get_future().get());
+  message.data = std::make_unique<uint8_t[]>(message.length);
+  for (auto& tensor : message.tensors) {
+    tensor.data = std::make_unique<uint8_t[]>(tensor.length);
+  }
+
+  std::promise<Message> readMessagePromise;
+  serverPipe->read(
+      std::move(message), [&](const Error& error, Message&& message) {
+        if (error) {
+          readMessagePromise.set_exception(
+              std::make_exception_ptr(std::runtime_error(error.what())));
+        } else {
+          readMessagePromise.set_value(std::move(message));
+        }
+      });
+
+  EXPECT_TRUE(
+      messagesAreEqual(readMessagePromise.get_future().get(), makeMessage()));
+  EXPECT_TRUE(messagesAreEqual(
+      writtenMessagePromise.get_future().get(), makeMessage()));
+
+  serverPipe.reset();
+  listener.reset();
+  clientPipe.reset();
+  context->join();
+}
+
+TEST(Context, ClientPingInline) {
+  std::promise<void> donePromise;
 
   auto context = Context::create();
 
@@ -71,74 +183,157 @@ TEST(Context, Simple) {
   context->registerChannelFactory(
       0, "basic", std::make_shared<channel::basic::BasicChannelFactory>());
 
-  std::vector<std::string> addresses;
-  addresses.push_back("shm://foobar");
-  addresses.push_back("uv://127.0.0.1");
-  auto listener = Listener::create(context, std::move(addresses));
+  auto listener = Listener::create(context, {"shm://foobar", "uv://127.0.0.1"});
 
+  std::shared_ptr<Pipe> serverPipe;
   listener->accept([&](const Error& error, std::shared_ptr<Pipe> pipe) {
     if (error) {
-      std::unique_lock<std::mutex> lock(mutex);
-      Defer d([&]() {
-        done = true;
-        cv.notify_all();
-      });
-      ASSERT_TRUE(false) << error.what();
+      ADD_FAILURE() << error.what();
+      donePromise.set_value();
+      return;
     }
-    pipe->readDescriptor([&, pipe](const Error& error, Message&& message) {
+    serverPipe = std::move(pipe);
+    serverPipe->readDescriptor([&](const Error& error, Message&& message) {
       if (error) {
-        std::unique_lock<std::mutex> lock(mutex);
-        Defer d([&]() {
-          done = true;
-          cv.notify_all();
-        });
-        ASSERT_TRUE(false) << error.what();
+        ADD_FAILURE() << error.what();
+        donePromise.set_value();
+        return;
       }
       message.data = std::make_unique<uint8_t[]>(message.length);
       for (auto& tensor : message.tensors) {
         tensor.data = std::make_unique<uint8_t[]>(tensor.length);
       }
-      pipe->read(
-          std::move(message), [&, pipe](const Error& error, Message&& message) {
+      serverPipe->read(
+          std::move(message), [&](const Error& error, Message&& message) {
             if (error) {
-              std::unique_lock<std::mutex> lock(mutex);
-              Defer d([&]() {
-                done = true;
-                cv.notify_all();
-              });
-              ASSERT_TRUE(false) << error.what();
+              ADD_FAILURE() << error.what();
+              donePromise.set_value();
+              return;
             }
-            std::unique_lock<std::mutex> lock(mutex);
-            Defer d([&]() {
-              done = true;
-              cv.notify_all();
-            });
-            ASSERT_TRUE(compareStringToBuffer(
-                messageData, message.data, message.length));
-            ASSERT_EQ(message.tensors.size(), 1);
-            ASSERT_TRUE(compareStringToBuffer(
-                tensorData,
-                message.tensors[0].data,
-                message.tensors[0].length));
+            EXPECT_TRUE(messagesAreEqual(message, makeMessage()));
+            donePromise.set_value();
           });
     });
   });
 
   auto clientPipe = Pipe::create(context, listener->url("uv"));
-  clientPipe->write(
-      std::move(sentMessage), [&](const Error& error, Message&& message) {
-        if (error) {
-          std::unique_lock<std::mutex> lock(mutex);
-          EXPECT_TRUE(false) << error.what();
-          cv.notify_all();
-        }
-      });
+  clientPipe->write(makeMessage(), [&](const Error& error, Message&& message) {
+    if (error) {
+      ADD_FAILURE() << error.what();
+      donePromise.set_value();
+      return;
+    }
+  });
 
-  std::unique_lock<std::mutex> lock(mutex);
-  while (!done) {
-    cv.wait(lock);
+  donePromise.get_future().get();
+
+  serverPipe.reset();
+  listener.reset();
+  clientPipe.reset();
+  context->join();
+}
+
+TEST(Context, ServerPingPongTwice) {
+  std::promise<void> donePromise;
+
+  auto context = Context::create();
+
+  context->registerTransport(
+      0, "uv", std::make_shared<transport::uv::Context>());
+  context->registerTransport(
+      -1, "shm", std::make_shared<transport::shm::Context>());
+  context->registerChannelFactory(
+      0, "basic", std::make_shared<channel::basic::BasicChannelFactory>());
+
+  auto listener = Listener::create(context, {"shm://foobar", "uv://127.0.0.1"});
+
+  std::shared_ptr<Pipe> serverPipe;
+
+  int numPingsGoneThrough = 0;
+  listener->accept([&](const Error& error, std::shared_ptr<Pipe> pipe) {
+    if (error) {
+      ADD_FAILURE() << error.what();
+      donePromise.set_value();
+      return;
+    }
+    serverPipe = std::move(pipe);
+    for (int i = 0; i < 2; i++) {
+      serverPipe->write(
+          makeMessage(),
+          [serverPipe, &donePromise, &numPingsGoneThrough, i](
+              const Error& error, Message&& message) {
+            if (error) {
+              ADD_FAILURE() << error.what();
+              donePromise.set_value();
+              return;
+            }
+            serverPipe->readDescriptor(
+                [serverPipe, &donePromise, &numPingsGoneThrough, i](
+                    const Error& error, Message&& message) {
+                  if (error) {
+                    ADD_FAILURE() << error.what();
+                    donePromise.set_value();
+                    return;
+                  }
+                  message.data = std::make_unique<uint8_t[]>(message.length);
+                  for (auto& tensor : message.tensors) {
+                    tensor.data = std::make_unique<uint8_t[]>(tensor.length);
+                  }
+                  serverPipe->read(
+                      std::move(message),
+                      [&donePromise, &numPingsGoneThrough, i](
+                          const Error& error, Message&& message) {
+                        if (error) {
+                          ADD_FAILURE() << error.what();
+                          donePromise.set_value();
+                          return;
+                        }
+                        EXPECT_TRUE(messagesAreEqual(message, makeMessage()));
+                        EXPECT_EQ(numPingsGoneThrough, i);
+                        numPingsGoneThrough++;
+                        if (numPingsGoneThrough == 2) {
+                          donePromise.set_value();
+                        }
+                      });
+                });
+          });
+    }
+  });
+
+  auto clientPipe = Pipe::create(context, listener->url("uv"));
+  for (int i = 0; i < 2; i++) {
+    clientPipe->readDescriptor([&](const Error& error, Message&& message) {
+      if (error) {
+        ADD_FAILURE() << error.what();
+        donePromise.set_value();
+        return;
+      }
+      message.data = std::make_unique<uint8_t[]>(message.length);
+      for (auto& tensor : message.tensors) {
+        tensor.data = std::make_unique<uint8_t[]>(tensor.length);
+      }
+      clientPipe->read(
+          std::move(message), [&](const Error& error, Message&& message) {
+            if (error) {
+              ADD_FAILURE() << error.what();
+              donePromise.set_value();
+              return;
+            }
+            clientPipe->write(
+                std::move(message), [&](const Error& error, Message&& message) {
+                  if (error) {
+                    ADD_FAILURE() << error.what();
+                    donePromise.set_value();
+                    return;
+                  }
+                });
+          });
+    });
   }
 
+  donePromise.get_future().get();
+
+  serverPipe.reset();
   listener.reset();
   clientPipe.reset();
   context->join();
