b940a629798433084f63d66a0c2c51e7afd77eb7
Lucas Hosseini <hoss@fb.com>
2020-04-06 09:08:44 +0200

    Factor out sanitizer options from root CMakeLists.txt.

---
+++ cmake/Sanitize.cmake
@@ -0,0 +1,11 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+if(SANITIZE)
+  add_definitions("-fsanitize=${SANITIZE}")
+  add_definitions("-fno-omit-frame-pointer")
+  set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -fsanitize=${SANITIZE}")
+endif()
