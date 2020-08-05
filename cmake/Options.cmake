d5170978bcbce7d3f12963566c10a719935e51f3
Luca Wehrstedt <lcw@fb.com>
2020-08-05 03:53:08 -0700

    Gate more optional features behind CMake flags (#196)
    Summary:
    Pull Request resolved: https://github.com/pytorch/tensorpipe/pull/196
    
    The idea here is that most people who build TensorPipe do it for the library itself, it's just us developers that need to build tests, or benchmarks, etc. So why not leave them disabled by default, so that there's no risk of users getting it wrong?
    
    Test Plan: Imported from OSS
    
    Reviewed By: malfet
    
    Differential Revision: D22928853
    
    Pulled By: lw
    
    fbshipit-source-id: 91db234d4a13db75edca4bb1329c471905785e06

--- cmake/Options.cmake
+++ cmake/Options.cmake
@@ -10,7 +10,16 @@ else()
   set(LINUX OFF)
 endif()
 
+# Transports
 option(TP_ENABLE_SHM "Enable shm transport" ${LINUX})
+
+# Channels
 option(TP_ENABLE_CMA "Enable cma channel" ${LINUX})
+
+# Optional features
+option(TP_BUILD_BENCHMARK "Build benchmarks" OFF)
 option(TP_BUILD_PYTHON "Build python bindings" OFF)
+option(TP_BUILD_TESTING "Build tests" OFF)
+
+# Force to build libuv from the included submodule
 option(TP_BUILD_LIBUV "Build libuv from source" OFF)
