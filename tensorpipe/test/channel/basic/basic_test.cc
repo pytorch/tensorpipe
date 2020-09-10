bd96dc724cc898fe95a9de81bb174223d5976ee8
Lucas Hosseini <lucas.hosseini@gmail.com>
2020-09-10 14:59:24 +0200

    Update on "Make Channel API accept tensor structs rather than raw pointers."
    + Introduce tensor.h defining the tensor struct(s). The `CpuTensor`
    struct is always defined, while the `CudaTensor` struct is defined
    only when `TENSORPIPE_HAS_CUDA` is true.
    + Update all channels to take a `CpuTensor` or `CudaTensor` for
    `send`/`recv` rather than a raw pointer and a length.
    + Make the base `Channel`/`Context` classes templated on `TTensor`,
    effectively creating two channel hierarchies (one for CPU channels,
    one for CUDA channels).
    + Update the Pipe and the generic channel tests to use the new API. So
    far, generic channel tests are CPU only, and tests for the CUDA IPC
    channel are (temporarily) disabled. A subsequent PR will take care of
    refactoring tests so that generic tests work for CUDA channels. An
    other PR will add support for CUDA tensors in the Pipe.
    
    Differential Revision: [D23598033](https://our.internmc.facebook.com/intern/diff/D23598033)
    
    [ghstack-poisoned]

--- tensorpipe/test/channel/basic/basic_test.cc
+++ tensorpipe/test/channel/basic/basic_test.cc
@@ -17,8 +17,8 @@ class BasicChannelTestHelper : public ChannelTestHelper {
     return "basic";
   }
 
-  std::shared_ptr<tensorpipe::channel::CpuContext>
-  makeContext(std::string id) override {
+  std::shared_ptr<tensorpipe::channel::CpuContext> makeContext(
+      std::string id) override {
     auto context = std::make_shared<tensorpipe::channel::basic::Context>();
     context->setId(std::move(id));
     return context;
--- tensorpipe/test/channel/xth/xth_test.cc
+++ tensorpipe/test/channel/xth/xth_test.cc
@@ -17,8 +17,8 @@ class XthChannelTestHelper : public ChannelTestHelper {
     return "xth";
   }
 
-  std::shared_ptr<tensorpipe::channel::CpuContext>
-  makeContext(std::string id) override {
+  std::shared_ptr<tensorpipe::channel::CpuContext> makeContext(
+      std::string id) override {
     auto context = std::make_shared<tensorpipe::channel::xth::Context>();
     context->setId(std::move(id));
     return context;
