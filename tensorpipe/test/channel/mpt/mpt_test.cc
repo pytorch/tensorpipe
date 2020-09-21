a44dd509cdd9de3e08a10e30bbef041cc413e507
Lucas Hosseini <lucas.hosseini@gmail.com>
2020-09-21 10:46:13 -0700

    Get rid of `channelName()` in channel test helper. (#213)
    Summary:
    Pull Request resolved: https://github.com/pytorch/tensorpipe/pull/213
    
    CUDA channel tests are currently disabled and this workaround will
    not be needed anymore with the upcoming test refactor.
    
    Differential Revision: D23598038
    
    Test Plan: Imported from OSS
    
    Reviewed By: lw
    
    Pulled By: beauby
    
    fbshipit-source-id: 7f2e8808a0689c6a97d4bf16a923ae6eca5fe867

--- tensorpipe/test/channel/mpt/mpt_test.cc
+++ tensorpipe/test/channel/mpt/mpt_test.cc
@@ -13,10 +13,6 @@ namespace {
 
 class MptChannelTestHelper : public ChannelTestHelper {
  public:
-  std::string channelName() override {
-    return "mpt";
-  }
-
   std::shared_ptr<tensorpipe::channel::CpuContext> makeContext(
       std::string id) override {
     std::vector<std::shared_ptr<tensorpipe::transport::Context>> contexts = {
