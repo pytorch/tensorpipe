e876534013fdabad555b14a21626964a2c161d93
Luca Wehrstedt <lcw@fb.com>
2020-10-15 01:49:38 -0700

    Add helpers for InfiniBand verbs API: RAII wrappers
    Differential Revision: D24136939
    
    fbshipit-source-id: 48e86b7b7101b24b061c31129169a56a54515286

--- tensorpipe/common/ibv.h
+++ tensorpipe/common/ibv.h
@@ -8,6 +8,8 @@
 
 #pragma once
 
+#include <memory>
+
 #include <infiniband/verbs.h>
 
 #include <tensorpipe/common/defs.h>
@@ -30,5 +32,126 @@ namespace tensorpipe {
   }
 
 #define TP_CHECK_IBV_VOID(op) op;
+
+// RAII wrappers
+
+class IbvDeviceList {
+ public:
+  IbvDeviceList()
+      : deviceList_(TP_CHECK_IBV_PTR(ibv_get_device_list(&size_))) {}
+
+  int size() {
+    return size_;
+  }
+
+  struct ibv_device& operator[](int i) {
+    return *deviceList_.get()[i];
+  }
+
+  void reset() {
+    deviceList_.reset();
+  }
+
+ private:
+  struct Deleter {
+    void operator()(struct ibv_device** ptr) const {
+      TP_CHECK_IBV_VOID(ibv_free_device_list(ptr));
+    }
+  };
+
+  std::unique_ptr<struct ibv_device*, Deleter> deviceList_;
+  int size_;
+};
+
+struct IbvContextDeleter {
+  void operator()(struct ibv_context* ptr) {
+    TP_CHECK_IBV_INT(ibv_close_device(ptr));
+  }
+};
+
+using IbvContext = std::unique_ptr<struct ibv_context, IbvContextDeleter>;
+
+inline IbvContext createIbvContext(struct ibv_device& device) {
+  return IbvContext(TP_CHECK_IBV_PTR(ibv_open_device(&device)));
+}
+
+struct IbvProtectionDomainDeleter {
+  void operator()(struct ibv_pd* ptr) {
+    TP_CHECK_IBV_INT(ibv_dealloc_pd(ptr));
+  }
+};
+
+using IbvProtectionDomain =
+    std::unique_ptr<struct ibv_pd, IbvProtectionDomainDeleter>;
+
+inline IbvProtectionDomain createIbvProtectionDomain(IbvContext& context) {
+  return IbvProtectionDomain(TP_CHECK_IBV_PTR(ibv_alloc_pd(context.get())));
+}
+
+struct IbvCompletionQueueDeleter {
+  void operator()(struct ibv_cq* ptr) {
+    TP_CHECK_IBV_INT(ibv_destroy_cq(ptr));
+  }
+};
+
+using IbvCompletionQueue =
+    std::unique_ptr<struct ibv_cq, IbvCompletionQueueDeleter>;
+
+inline IbvCompletionQueue createIbvCompletionQueue(
+    IbvContext& context,
+    int cqe,
+    void* cq_context,
+    struct ibv_comp_channel* channel,
+    int comp_vector) {
+  return IbvCompletionQueue(TP_CHECK_IBV_PTR(
+      ibv_create_cq(context.get(), cqe, cq_context, channel, comp_vector)));
+}
+
+struct IbvSharedReceiveQueueDeleter {
+  void operator()(struct ibv_srq* ptr) {
+    TP_CHECK_IBV_INT(ibv_destroy_srq(ptr));
+  }
+};
+
+using IbvSharedReceiveQueue =
+    std::unique_ptr<struct ibv_srq, IbvSharedReceiveQueueDeleter>;
+
+inline IbvSharedReceiveQueue createIbvSharedReceiveQueue(
+    IbvProtectionDomain& pd,
+    struct ibv_srq_init_attr& initAttr) {
+  return IbvSharedReceiveQueue(
+      TP_CHECK_IBV_PTR(ibv_create_srq(pd.get(), &initAttr)));
+}
+
+struct IbvMemoryRegionDeleter {
+  void operator()(struct ibv_mr* ptr) {
+    TP_CHECK_IBV_INT(ibv_dereg_mr(ptr));
+  }
+};
+
+using IbvMemoryRegion = std::unique_ptr<struct ibv_mr, IbvMemoryRegionDeleter>;
+
+inline IbvMemoryRegion createIbvMemoryRegion(
+    IbvProtectionDomain& pd,
+    void* addr,
+    size_t length,
+    int accessFlags) {
+  return IbvMemoryRegion(
+      TP_CHECK_IBV_PTR(ibv_reg_mr(pd.get(), addr, length, accessFlags)));
+}
+
+struct IbvQueuePairDeleter {
+  void operator()(struct ibv_qp* ptr) {
+    TP_CHECK_IBV_INT(ibv_destroy_qp(ptr));
+  }
+};
+
+using IbvQueuePair = std::unique_ptr<struct ibv_qp, IbvQueuePairDeleter>;
+
+inline IbvQueuePair createIbvQueuePair(
+    IbvProtectionDomain& pd,
+    struct ibv_qp_init_attr& initAttr) {
+  return IbvQueuePair(TP_CHECK_IBV_PTR(ibv_create_qp(pd.get(), &initAttr)));
+}
 
 } // namespace tensorpipe
