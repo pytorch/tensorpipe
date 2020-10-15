96074417015a4201c58a12002be137eb48a7305d
Luca Wehrstedt <lcw@fb.com>
2020-10-15 09:12:41 -0700

    Add helpers for InfiniBand verbs API: queue pair creation and state transitions
    Summary: Add some helpers to create a queue pair and transition it between its states. The values for most parameters were copied from the  ones used by Gloo in [this file](https://github.com/facebookincubator/gloo/blob/master/gloo/transport/ibverbs/pair.cc).
    
    Reviewed By: beauby, tscmoo
    
    Differential Revision: D24136940
    
    fbshipit-source-id: bb2b5af532ca112ffd61c35b2532664fd29252bb

--- tensorpipe/common/ibv.cc
+++ tensorpipe/common/ibv.cc
@@ -49,4 +49,114 @@ struct IbvSetupInformation makeIbvSetupInformation(
   return info;
 }
 
+void transitionIbvQueuePairToInit(IbvQueuePair& qp, IbvAddress& selfAddr) {
+  struct ibv_qp_attr attr;
+  std::memset(&attr, 0, sizeof(attr));
+  int attrMask = 0;
+
+  attrMask |= IBV_QP_STATE;
+  attr.qp_state = IBV_QPS_INIT;
+
+  // Hardcode the use of the first entry of the partition key table, as it will
+  // always be valid.
+  // FIXME: Make this configurable similarly to the port number.
+  attrMask |= IBV_QP_PKEY_INDEX;
+  attr.pkey_index = 0;
+
+  attrMask |= IBV_QP_PORT;
+  attr.port_num = selfAddr.portNum;
+
+  attrMask |= IBV_QP_ACCESS_FLAGS;
+  attr.qp_access_flags = IBV_ACCESS_LOCAL_WRITE | IBV_ACCESS_REMOTE_WRITE;
+
+  TP_CHECK_IBV_INT(ibv_modify_qp(qp.get(), &attr, attrMask));
+}
+
+void transitionIbvQueuePairToReadyToReceive(
+    IbvQueuePair& qp,
+    IbvAddress& selfAddr,
+    IbvSetupInformation& destinationInfo) {
+  struct ibv_qp_attr attr;
+  std::memset(&attr, 0, sizeof(attr));
+  int attrMask = 0;
+
+  attrMask |= IBV_QP_STATE;
+  attr.qp_state = IBV_QPS_RTR;
+
+  // Global routing is only set up as far as needed to support RoCE.
+  attrMask |= IBV_QP_AV;
+  if (destinationInfo.localIdentifier != 0) {
+    attr.ah_attr.is_global = 0;
+    attr.ah_attr.dlid = destinationInfo.localIdentifier;
+  } else {
+    attr.ah_attr.is_global = 1;
+    attr.ah_attr.grh.dgid = destinationInfo.globalIdentifier;
+    attr.ah_attr.grh.sgid_index = selfAddr.globalIdentifierIndex;
+    attr.ah_attr.grh.hop_limit = 1;
+  }
+  attr.ah_attr.port_num = selfAddr.portNum;
+
+  attrMask |= IBV_QP_PATH_MTU;
+  attr.path_mtu = std::min(
+      selfAddr.maximumTransmissionUnit,
+      destinationInfo.maximumTransmissionUnit);
+
+  attrMask |= IBV_QP_DEST_QPN;
+  attr.dest_qp_num = destinationInfo.queuePairNumber;
+
+  // The packet sequence numbers of the local send and of the remote receive
+  // queues (and vice versa) only need to match. Thus we set them all to zero.
+  attrMask |= IBV_QP_RQ_PSN;
+  attr.rq_psn = 0;
+
+  attrMask |= IBV_QP_MAX_DEST_RD_ATOMIC;
+  attr.max_dest_rd_atomic = 1;
+
+  attrMask |= IBV_QP_MIN_RNR_TIMER;
+  attr.min_rnr_timer = 20; // 10.24 milliseconds
+
+  TP_CHECK_IBV_INT(ibv_modify_qp(qp.get(), &attr, attrMask));
+}
+
+void transitionIbvQueuePairToReadyToSend(
+    IbvQueuePair& qp,
+    IbvSetupInformation& selfInfo) {
+  struct ibv_qp_attr attr;
+  std::memset(&attr, 0, sizeof(attr));
+  int attrMask = 0;
+
+  attrMask |= IBV_QP_STATE;
+  attr.qp_state = IBV_QPS_RTS;
+
+  // The packet sequence numbers of the local send and of the remote receive
+  // queues (and vice versa) only need to match. Thus we set them all to zero.
+  attrMask |= IBV_QP_SQ_PSN;
+  attr.sq_psn = 0;
+
+  attrMask |= IBV_QP_TIMEOUT;
+  attr.timeout = 14; // 67.1 milliseconds
+
+  attrMask |= IBV_QP_RETRY_CNT;
+  attr.retry_cnt = 7;
+
+  attrMask |= IBV_QP_RNR_RETRY;
+  attr.rnr_retry = 7; // infinite
+
+  attrMask |= IBV_QP_MAX_QP_RD_ATOMIC;
+  attr.max_rd_atomic = 1;
+
+  TP_CHECK_IBV_INT(ibv_modify_qp(qp.get(), &attr, attrMask));
+}
+
+void transitionIbvQueuePairToError(IbvQueuePair& qp) {
+  struct ibv_qp_attr attr;
+  std::memset(&attr, 0, sizeof(attr));
+  int attrMask = 0;
+
+  attrMask |= IBV_QP_STATE;
+  attr.qp_state = IBV_QPS_ERR;
+
+  TP_CHECK_IBV_INT(ibv_modify_qp(qp.get(), &attr, attrMask));
+}
+
 } // namespace tensorpipe
