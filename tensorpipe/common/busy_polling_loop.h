eb7f6d1bebca3bd7c9a37c233c86d562623ead32
Luca Wehrstedt <lcw@fb.com>
2020-10-28 00:57:50 -0700

    Merge busy polling loops of SHM and IBV in common helper
    Summary: SHM and IBV both had the same busy-polling logic: first poll their "proper" work (reactions, work completions, ...), if nothing is found then check for deferred functions, and if still nothing is found then yield. This was after all a rather thin logic, but still somewhat controversial, especially the yielding part. To make sure this specific logic is clear and visible (and that updates to it are consistently applied to all transports), we extract it to its own class.
    
    Reviewed By: beauby
    
    Differential Revision: D24444979
    
    fbshipit-source-id: e1d7289c20c27aedca8e3a0e72e08cb455dffbc2

---
+++ tensorpipe/common/busy_polling_loop.h
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <atomic>
+#include <string>
+#include <thread>
+#include <utility>
+
+#include <tensorpipe/common/deferred_executor.h>
+#include <tensorpipe/common/system.h>
+
+namespace tensorpipe {
+
+class BusyPollingLoop : public EventLoopDeferredExecutor {
+ protected:
+  virtual bool pollOnce() = 0;
+
+  virtual bool readyToClose() = 0;
+
+  void stopBusyPolling() {
+    closed_ = true;
+    // No need to wake up the thread, since it is busy-waiting.
+  }
+
+  void eventLoop() override {
+    while (!closed_ || !readyToClose()) {
+      if (pollOnce()) {
+        // continue
+      } else if (deferredFunctionCount_ > 0) {
+        deferredFunctionCount_ -= runDeferredFunctionsFromEventLoop();
+      } else {
+        std::this_thread::yield();
+      }
+    }
+  };
+
+  void wakeupEventLoopToDeferFunction() override {
+    ++deferredFunctionCount_;
+    // No need to wake up the thread, since it is busy-waiting.
+  };
+
+ private:
+  std::atomic<bool> closed_{false};
+
+  std::atomic<int64_t> deferredFunctionCount_{0};
+};
+
+} // namespace tensorpipe
