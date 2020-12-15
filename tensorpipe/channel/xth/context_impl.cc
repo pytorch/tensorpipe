3eb96ce78d1939adf464838146972cdaa6ec0430
Luca Wehrstedt <lcw@fb.com>
2020-12-15 07:15:54 -0800

    Use boilerplate in XTH channel
    Differential Revision: D25395582
    
    fbshipit-source-id: 9fd4b20cbbf6d6b8dbebc32b363ef47e39d6d9d9

---
+++ tensorpipe/channel/xth/context_impl.cc
@@ -0,0 +1,119 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <tensorpipe/channel/xth/context_impl.h>
+
+#include <unistd.h>
+
+#include <cstring>
+#include <functional>
+#include <limits>
+#include <sstream>
+#include <string>
+#include <thread>
+#include <utility>
+
+#include <tensorpipe/channel/xth/channel_impl.h>
+#include <tensorpipe/common/defs.h>
+#include <tensorpipe/common/system.h>
+
+namespace tensorpipe {
+namespace channel {
+namespace xth {
+
+namespace {
+
+std::string generateDomainDescriptor() {
+  std::ostringstream oss;
+  auto bootID = getBootID();
+  TP_THROW_ASSERT_IF(!bootID) << "Unable to read boot_id";
+
+  pid_t pid = getpid();
+
+  // Combine boot ID and PID.
+  oss << bootID.value() << "-" << pid;
+
+  return oss.str();
+}
+
+} // namespace
+
+ContextImpl::ContextImpl()
+    : ContextImplBoilerplate<CpuBuffer, ContextImpl, ChannelImpl>(
+          generateDomainDescriptor()),
+      requests_(std::numeric_limits<int>::max()) {
+  thread_ = std::thread(&ContextImpl::handleCopyRequests, this);
+}
+
+std::shared_ptr<CpuChannel> ContextImpl::createChannel(
+    std::shared_ptr<transport::Connection> connection,
+    Endpoint /* unused */) {
+  return createChannelInternal(std::move(connection));
+}
+
+void ContextImpl::closeImpl() {
+  requests_.push(nullopt);
+}
+
+void ContextImpl::joinImpl() {
+  thread_.join();
+  // TP_DCHECK(requests_.empty());
+}
+
+bool ContextImpl::inLoop() {
+  return loop_.inLoop();
+};
+
+void ContextImpl::deferToLoop(std::function<void()> fn) {
+  loop_.deferToLoop(std::move(fn));
+};
+
+void ContextImpl::requestCopy(
+    void* remotePtr,
+    void* localPtr,
+    size_t length,
+    std::function<void(const Error&)> fn) {
+  uint64_t requestId = nextRequestId_++;
+  TP_VLOG(4) << "Channel context " << id_ << " received a copy request (#"
+             << requestId << ")";
+
+  fn = [this, requestId, fn{std::move(fn)}](const Error& error) {
+    TP_VLOG(4) << "Channel context " << id_
+               << " is calling a copy request callback (#" << requestId << ")";
+    fn(error);
+    TP_VLOG(4) << "Channel context " << id_
+               << " done calling a copy request callback (#" << requestId
+               << ")";
+  };
+
+  requests_.push(CopyRequest{remotePtr, localPtr, length, std::move(fn)});
+}
+
+void ContextImpl::handleCopyRequests() {
+  setThreadName("TP_XTH_loop");
+  while (true) {
+    auto maybeRequest = requests_.pop();
+    if (!maybeRequest.has_value()) {
+      break;
+    }
+    CopyRequest request = std::move(maybeRequest).value();
+
+    // Don't even call memcpy on a length of 0 to avoid issues with the pointer
+    // possibly being null.
+    if (request.length > 0) {
+      // Perform copy.
+      std::memcpy(request.localPtr, request.remotePtr, request.length);
+    }
+
+    request.callback(Error::kSuccess);
+  }
+}
+
+} // namespace xth
+} // namespace channel
+} // namespace tensorpipe
