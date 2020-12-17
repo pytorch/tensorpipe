4f81210c1359f4ce17a0be56be6c94b424bc966c
Luca Wehrstedt <lcw@fb.com>
2020-12-17 06:00:27 -0800

    Introduce channel boilerplate
    Summary:
    Re-do for channels what we did for transports a while back.
    
    All channels need to do with some basic functionality like defining an impl, setting up an event loop, deferring everything to it, handle errors, ... As over time we have developed best practices to do so, rather than copy-paste them into each channel (and bloat them, and thus hide the "core" logic) we merge them all into a common superclass.
    
    Note that this boilerplate doesn't yet contain the latest fixes we have adopted for transports with respect to closing. I'll do so very shortly.
    
    This diff also doesn't migrate any of the current channels to this boilerplate. I'll do so, one by one, in the subsequent diffs.
    
    Reviewed By: beauby
    
    Differential Revision: D25395585
    
    fbshipit-source-id: 091c46598ae72fc3f603f1734c7a73a79b983229

---
+++ tensorpipe/channel/context_impl_boilerplate.h
@@ -0,0 +1,174 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <atomic>
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <utility>
+
+#include <tensorpipe/channel/channel_boilerplate.h>
+#include <tensorpipe/common/callback.h>
+#include <tensorpipe/common/defs.h>
+#include <tensorpipe/transport/context.h>
+
+namespace tensorpipe {
+namespace channel {
+
+template <typename TBuffer, typename TCtx, typename TChan>
+class ContextImplBoilerplate : public virtual DeferredExecutor,
+                               public std::enable_shared_from_this<TCtx> {
+ public:
+  explicit ContextImplBoilerplate(std::string domainDescriptor);
+
+  ContextImplBoilerplate(const ContextImplBoilerplate&) = delete;
+  ContextImplBoilerplate(ContextImplBoilerplate&&) = delete;
+  ContextImplBoilerplate& operator=(const ContextImplBoilerplate&) = delete;
+  ContextImplBoilerplate& operator=(ContextImplBoilerplate&&) = delete;
+
+  const std::string& domainDescriptor() const;
+
+  // Enrolling dependent objects (channels) causes them to be kept alive for as
+  // long as the context exists. These objects should enroll themselves as soon
+  // as they're created (in their initImplFromLoop method) and unenroll
+  // themselves after they've completed handling an error (either right in the
+  // handleErrorImpl method or in a subsequent callback). The context, on the
+  // other hand, should avoid terminating (i.e., complete joining) until all
+  // objects have unenrolled themselves.
+  void enroll(TChan& channel);
+  void unenroll(TChan& channel);
+
+  ClosingEmitter& getClosingEmitter();
+
+  void setId(std::string id);
+
+  void close();
+
+  void join();
+
+  virtual ~ContextImplBoilerplate() = default;
+
+ protected:
+  virtual void closeImpl() = 0;
+  virtual void joinImpl() = 0;
+  virtual void setIdImpl() {}
+
+  template <typename... Args>
+  std::shared_ptr<Channel<TBuffer>> createChannelInternal(Args&&... args);
+
+  // An identifier for the context, composed of the identifier for the context,
+  // combined with the channel's name. It will only be used for logging and
+  // debugging purposes.
+  std::string id_{"N/A"};
+
+ private:
+  std::atomic<bool> closed_{false};
+  std::atomic<bool> joined_{false};
+  ClosingEmitter closingEmitter_;
+
+  const std::string domainDescriptor_;
+
+  // Sequence numbers for the channels created by this context, used to create
+  // their identifiers based off this context's identifier. They will only be
+  // used for logging and debugging.
+  std::atomic<uint64_t> channelCounter_{0};
+
+  // Store shared_ptrs to dependent objects that have enrolled themselves to
+  // keep them alive. We use a map, indexed by raw pointers, rather than a set
+  // of shared_ptrs so that we can erase objects without them having to create
+  // a fresh shared_ptr just for that.
+  std::unordered_map<TChan*, std::shared_ptr<TChan>> channels_;
+};
+
+template <typename TBuffer, typename TCtx, typename TChan>
+ContextImplBoilerplate<TBuffer, TCtx, TChan>::ContextImplBoilerplate(
+    std::string domainDescriptor)
+    : domainDescriptor_(std::move(domainDescriptor)) {}
+
+template <typename TBuffer, typename TCtx, typename TChan>
+template <typename... Args>
+std::shared_ptr<Channel<TBuffer>> ContextImplBoilerplate<TBuffer, TCtx, TChan>::
+    createChannelInternal(Args&&... args) {
+  std::string channelId = id_ + ".c" + std::to_string(channelCounter_++);
+  TP_VLOG(4) << "Channel context " << id_ << " is opening channel "
+             << channelId;
+  return std::make_shared<ChannelBoilerplate<TBuffer, TCtx, TChan>>(
+      typename ChannelImplBoilerplate<TBuffer, TCtx, TChan>::ConstructorToken(),
+      this->shared_from_this(),
+      std::move(channelId),
+      std::forward<Args>(args)...);
+}
+
+template <typename TBuffer, typename TCtx, typename TChan>
+const std::string& ContextImplBoilerplate<TBuffer, TCtx, TChan>::
+    domainDescriptor() const {
+  return domainDescriptor_;
+}
+
+template <typename TBuffer, typename TCtx, typename TChan>
+void ContextImplBoilerplate<TBuffer, TCtx, TChan>::enroll(TChan& channel) {
+  TP_DCHECK(inLoop());
+  bool wasInserted;
+  std::tie(std::ignore, wasInserted) =
+      channels_.emplace(&channel, channel.shared_from_this());
+  TP_DCHECK(wasInserted);
+}
+
+template <typename TBuffer, typename TCtx, typename TChan>
+void ContextImplBoilerplate<TBuffer, TCtx, TChan>::unenroll(TChan& channel) {
+  TP_DCHECK(inLoop());
+  auto numRemoved = channels_.erase(&channel);
+  TP_DCHECK_EQ(numRemoved, 1);
+}
+
+template <typename TBuffer, typename TCtx, typename TChan>
+ClosingEmitter& ContextImplBoilerplate<TBuffer, TCtx, TChan>::
+    getClosingEmitter() {
+  return closingEmitter_;
+};
+
+template <typename TBuffer, typename TCtx, typename TChan>
+void ContextImplBoilerplate<TBuffer, TCtx, TChan>::setId(std::string id) {
+  TP_VLOG(4) << "Channel context " << id_ << " was renamed to " << id;
+  id_ = std::move(id);
+  setIdImpl();
+}
+
+template <typename TBuffer, typename TCtx, typename TChan>
+void ContextImplBoilerplate<TBuffer, TCtx, TChan>::close() {
+  if (!closed_.exchange(true)) {
+    TP_VLOG(4) << "Channel context " << id_ << " is closing";
+
+    closingEmitter_.close();
+    closeImpl();
+
+    TP_VLOG(4) << "Channel context " << id_ << " done closing";
+  }
+}
+
+template <typename TBuffer, typename TCtx, typename TChan>
+void ContextImplBoilerplate<TBuffer, TCtx, TChan>::join() {
+  close();
+
+  if (!joined_.exchange(true)) {
+    TP_VLOG(4) << "Channel context " << id_ << " is joining";
+
+    joinImpl();
+
+    TP_VLOG(4) << "Channel context " << id_ << " done joining";
+
+    // FIXME This may actually not be true, as channels could for example be
+    // kept alive by the underlying transport, and thus outlive their context.
+    // TP_DCHECK(channels_.empty());
+  }
+}
+
+} // namespace channel
+} // namespace tensorpipe
