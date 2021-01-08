9b675f688330c8612887ab52a1ea6cc4a003c30c
Luca Wehrstedt <lcw@fb.com>
2021-01-08 02:56:46 -0800

    Restore CUDA GDR viability detection
    Differential Revision: D25846533
    
    fbshipit-source-id: 34cdefe7c26c11d7c8a58f62ed8bd905ba41a115

--- tensorpipe/channel/cuda_gdr/context.h
+++ tensorpipe/channel/cuda_gdr/context.h
@@ -36,6 +36,8 @@ class Context : public CudaContext {
 
   const std::string& domainDescriptor() const override;
 
+  bool isViable() const override;
+
   void setId(std::string id) override;
 
   void close() override;
