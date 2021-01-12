0d65bceeab5149f458bc2b43b129fac2c4836070
mrshenli <cs.shenli@gmail.com>
2021-01-12 13:47:55 -0500

    fix lint

--- tensorpipe/channel/cuda_ipc/channel_impl.cc
+++ tensorpipe/channel/cuda_ipc/channel_impl.cc
@@ -154,9 +154,9 @@ void ChannelImpl::recvImplFromLoop(
     TDescriptor descriptor,
     CudaBuffer buffer,
     TRecvCallback callback) {
-  // Need to guard otherwise some op on the receiver will crash. 
-  // TODO: figure out which CUDA op crashed and replace this with a 
-  // more precise fix. 
+  // Need to guard otherwise some op on the receiver will crash.
+  // TODO: figure out which CUDA op crashed and replace this with a
+  // more precise fix.
   CudaDeviceGuard guard(cudaDeviceForPointer(buffer.ptr));
   recvOperations_.emplace_back(
       sequenceNumber, buffer.ptr, buffer.stream, buffer.length);
