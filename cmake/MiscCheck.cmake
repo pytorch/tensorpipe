7957811ee325f99a84bb3c60f9e69b7855dbe70f
Rong Rong <rongr@fb.com>
2021-02-23 11:57:38 -0800

    add checker for linux only

--- cmake/MiscCheck.cmake
+++ cmake/MiscCheck.cmake
@@ -9,39 +9,41 @@ if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
   endif()
 endif()
 
-# Check libc contains process_vm_readv
-CMAKE_PUSH_CHECK_STATE(RESET)
-set(CMAKE_REQUIRED_FLAGS "${CMAKE_CXX_FLAGS}")
-CHECK_CXX_SOURCE_COMPILES("
-#include <sys/uio.h>
-#include <sys/types.h>
-#include <unistd.h>
+if(LINUX)
+  # Check libc contains process_vm_readv
+  CMAKE_PUSH_CHECK_STATE(RESET)
+  set(CMAKE_REQUIRED_FLAGS "${CMAKE_CXX_FLAGS}")
+  CHECK_CXX_SOURCE_COMPILES("
+    #include <sys/uio.h>
+    #include <sys/types.h>
+    #include <unistd.h>
 
-namespace {
-bool isProcessVmReadvSyscallAllowed() {
-  long long someSourceValue = 0x0123456789abcdef;
-  long long someTargetValue = 0;
-  struct iovec source = {
-    .iov_base = &someSourceValue, .iov_len = sizeof(someSourceValue)
-  };
-  struct iovec target = {
-    .iov_base = &someTargetValue, .iov_len = sizeof(someTargetValue)
-  };
-  ssize_t nread = ::process_vm_readv(::getpid(), &target, 1, &source, 1, 0);
-  return nread == sizeof(long long) && someTargetValue == someSourceValue;
-}
-}
+    namespace {
+    bool isProcessVmReadvSyscallAllowed() {
+      long long someSourceValue = 0x0123456789abcdef;
+      long long someTargetValue = 0;
+      struct iovec source = {
+        .iov_base = &someSourceValue, .iov_len = sizeof(someSourceValue)
+      };
+      struct iovec target = {
+        .iov_base = &someTargetValue, .iov_len = sizeof(someTargetValue)
+      };
+      ssize_t nread = ::process_vm_readv(::getpid(), &target, 1, &source, 1, 0);
+      return nread == sizeof(long long) && someTargetValue == someSourceValue;
+    }
+    }
 
-int main() {
-  isProcessVmReadvSyscallAllowed();
-  return 0;
-}" SUPPORT_GLIBCXX_USE_PROCESS_VM_READV)
-if(NOT SUPPORT_GLIBCXX_USE_PROCESS_VM_READV)
-  unset(SUPPORT_GLIBCXX_USE_PROCESS_VM_READV CACHE)
-  message(FATAL_ERROR
-      "The C++ compiler does not support required functions. "
-      "This likely due to an old version of libc.so is used. "
-      "Please check your system linker setting.")
+    int main() {
+      isProcessVmReadvSyscallAllowed();
+      return 0;
+    }" SUPPORT_GLIBCXX_USE_PROCESS_VM_READV)
+  if(NOT SUPPORT_GLIBCXX_USE_PROCESS_VM_READV)
+    unset(SUPPORT_GLIBCXX_USE_PROCESS_VM_READV CACHE)
+    message(FATAL_ERROR
+        "The C++ compiler does not support required functions. "
+        "This likely due to an old version of libc.so is used. "
+        "Please check your system linker setting.")
+  endif()
+  CMAKE_POP_CHECK_STATE()
 endif()
-CMAKE_POP_CHECK_STATE()
 
