4632c61ae857467f14749a8554cb53324ac62be4
Lucas Hosseini <hoss@fb.com>
2021-04-02 08:37:57 -0700

    Introduce {Cpu,Cuda}Buffer::getDevice().
    Summary:
    In order to pick a channel for a given tensor, the Pipe will need to
    figure out which device the tensor resides on. Since we want the Pipe to be
    device-agnostic, this logic is implemented as a method on the `{Cpu,Cuda}Buffer`
    structs.
    For the CPU case, it is quite straightforward, as we see all CPU memory as
    residing on CPU 0.
    For the CUDA case, we need to figure out the device index from the pointer,
    which requires having access to an instance of `CudaLib`. Since the `CudaBuffer`
    struct is handled directly by the the Pipe (which does not know about CUDA), we
    cannot make the `getDevice()` method take a `CudaLib&` parameter as we usually
    do. Instead, we lazily instantiate a `static CudaLib` variable upon calling
    `CudaBuffer::getDevice()`.
    
    Reviewed By: lw
    
    Differential Revision: D27223204
    
    fbshipit-source-id: 7ea86676a4681069b4c77f02db7c3c4197cc041a

---
+++ tensorpipe/common/cuda_buffer.cc
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <tensorpipe/common/cuda_buffer.h>
+
+#include <tensorpipe/common/cuda.h>
+#include <tensorpipe/common/defs.h>
+
+namespace tensorpipe {
+
+Device CudaBuffer::getDevice() const {
+  static CudaLib cudaLib = []() {
+    Error error;
+    CudaLib lib;
+    std::tie(error, lib) = CudaLib::create();
+    TP_THROW_ASSERT_IF(error)
+        << "Cannot get CUDA device for pointer because libcuda could not be loaded: "
+        << error.what();
+    return lib;
+  }();
+
+  return Device{kCudaDeviceType, cudaDeviceForPointer(cudaLib, ptr)};
+}
+
+} // namespace tensorpipe
