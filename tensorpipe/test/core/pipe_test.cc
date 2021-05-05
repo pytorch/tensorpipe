286c2e2ce66b0d1408538a4e72f4971bf53ceaee
Lucas Hosseini <hoss@fb.com>
2021-05-05 03:52:43 -0700

    Add tests for the Pipe. (#369)
    Summary: Pull Request resolved: https://github.com/pytorch/tensorpipe/pull/369
    
    Reviewed By: lw
    
    Differential Revision: D27771657
    
    Pulled By: beauby
    
    fbshipit-source-id: 679bf0d4102e9b5f646b339b88984a0f594c5bb2

---
+++ tensorpipe/test/core/pipe_test.cc
@@ -0,0 +1,446 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#include <tensorpipe/test/core/pipe_test.h>
+
+using namespace tensorpipe;
+
+class SimpleWriteReadTest : public ClientServerPipeTestCase {
+  InlineMessage imessage_ = {
+      .payloads =
+          {
+              {.data = "payload #1", .metadata = "payload metadata #1"},
+              {.data = "payload #2", .metadata = "payload metadata #2"},
+              {.data = "payload #3", .metadata = "payload metadata #3"},
+          },
+      .tensors =
+          {
+              {
+                  .data = "tensor #1",
+                  .metadata = "tensor metadata #1",
+                  .device = Device{kCpuDeviceType, 0},
+              },
+              {
+                  .data = "tensor #2",
+                  .metadata = "tensor metadata #2",
+                  .device = Device{kCpuDeviceType, 0},
+              },
+              {
+                  .data = "tensor #3",
+                  .metadata = "tensor metadata #3",
+                  .device = Device{kCpuDeviceType, 0},
+              },
+          },
+      .metadata = "pipe metadata",
+  };
+
+ public:
+  void server(Pipe& pipe) override {
+    Message message;
+    Storage storage;
+    std::tie(message, storage) = makeMessage(imessage_);
+    auto future = pipeWriteWithFuture(pipe, message);
+    future.get();
+  }
+
+  void client(Pipe& pipe) override {
+    Descriptor descriptor;
+    Storage storage;
+    auto future = pipeReadWithFuture(
+        pipe,
+        /*targetDevices=*/
+        {
+            Device{kCpuDeviceType, 0},
+            Device{kCpuDeviceType, 0},
+            Device{kCpuDeviceType, 0},
+        });
+    std::tie(descriptor, storage) = future.get();
+    expectDescriptorAndStorageMatchMessage(descriptor, storage, imessage_);
+  }
+};
+
+TEST(Pipe, SimpleWriteRead) {
+  SimpleWriteReadTest test;
+  test.run();
+}
+
+class SimpleWriteReadPayloadsOnlyTest : public ClientServerPipeTestCase {
+  InlineMessage imessage_ = {
+      .payloads =
+          {
+              {.data = "payload #1", .metadata = "payload metadata #1"},
+              {.data = "payload #2", .metadata = "payload metadata #2"},
+              {.data = "payload #3", .metadata = "payload metadata #3"},
+          },
+      .tensors = {},
+      .metadata = "pipe metadata",
+  };
+
+ public:
+  void server(Pipe& pipe) override {
+    Message message;
+    Storage storage;
+    std::tie(message, storage) = makeMessage(imessage_);
+    auto future = pipeWriteWithFuture(pipe, message);
+    future.get();
+  }
+
+  void client(Pipe& pipe) override {
+    Descriptor descriptor;
+    Storage storage;
+    auto future = pipeReadWithFuture(pipe, /*targetDevices=*/{});
+    std::tie(descriptor, storage) = future.get();
+    expectDescriptorAndStorageMatchMessage(descriptor, storage, imessage_);
+  }
+};
+
+TEST(Pipe, SimpleWriteReadPayloadsOnly) {
+  SimpleWriteReadPayloadsOnlyTest test;
+  test.run();
+}
+
+class SimpleWriteReadTensorsOnlyTest : public ClientServerPipeTestCase {
+  InlineMessage imessage_ = {
+      .payloads = {},
+      .tensors =
+          {
+              {
+                  .data = "tensor #1",
+                  .metadata = "tensor metadata #1",
+                  .device = Device{kCpuDeviceType, 0},
+              },
+              {
+                  .data = "tensor #2",
+                  .metadata = "tensor metadata #2",
+                  .device = Device{kCpuDeviceType, 0},
+              },
+              {
+                  .data = "tensor #3",
+                  .metadata = "tensor metadata #3",
+                  .device = Device{kCpuDeviceType, 0},
+              },
+          },
+      .metadata = "pipe metadata",
+  };
+
+ public:
+  void server(Pipe& pipe) override {
+    Message message;
+    Storage storage;
+    std::tie(message, storage) = makeMessage(imessage_);
+    auto future = pipeWriteWithFuture(pipe, message);
+    future.get();
+  }
+
+  void client(Pipe& pipe) override {
+    Descriptor descriptor;
+    Storage storage;
+    auto future = pipeReadWithFuture(
+        pipe,
+        /*targetDevices=*/
+        {
+            Device{kCpuDeviceType, 0},
+            Device{kCpuDeviceType, 0},
+            Device{kCpuDeviceType, 0},
+        });
+    std::tie(descriptor, storage) = future.get();
+    expectDescriptorAndStorageMatchMessage(descriptor, storage, imessage_);
+  }
+};
+
+TEST(Pipe, SimpleWriteReadTensorsOnly) {
+  SimpleWriteReadTensorsOnlyTest test;
+  test.run();
+}
+
+class SimpleWriteReadWithAllTargetDevicesTest
+    : public ClientServerPipeTestCase {
+  InlineMessage imessage_ = {
+      .payloads =
+          {
+              {.data = "payload #1", .metadata = "payload metadata #1"},
+              {.data = "payload #2", .metadata = "payload metadata #2"},
+              {.data = "payload #3", .metadata = "payload metadata #3"},
+          },
+      .tensors =
+          {
+              {
+                  .data = "tensor #1",
+                  .metadata = "tensor metadata #1",
+                  .device = Device{kCpuDeviceType, 0},
+                  .targetDevice = Device{kCpuDeviceType, 0},
+              },
+              {
+                  .data = "tensor #2",
+                  .metadata = "tensor metadata #2",
+                  .device = Device{kCpuDeviceType, 0},
+                  .targetDevice = Device{kCpuDeviceType, 0},
+              },
+              {
+                  .data = "tensor #3",
+                  .metadata = "tensor metadata #3",
+                  .device = Device{kCpuDeviceType, 0},
+                  .targetDevice = Device{kCpuDeviceType, 0},
+              },
+          },
+      .metadata = "pipe metadata",
+  };
+
+ public:
+  void server(Pipe& pipe) override {
+    Message message;
+    Storage storage;
+    std::tie(message, storage) = makeMessage(imessage_);
+    auto future = pipeWriteWithFuture(pipe, message);
+    future.get();
+  }
+
+  void client(Pipe& pipe) override {
+    Descriptor descriptor;
+    Storage storage;
+    auto future = pipeReadWithFuture(
+        pipe,
+        /*targetDevices=*/
+        {
+            Device{kCpuDeviceType, 0},
+            Device{kCpuDeviceType, 0},
+            Device{kCpuDeviceType, 0},
+        });
+    std::tie(descriptor, storage) = future.get();
+    expectDescriptorAndStorageMatchMessage(descriptor, storage, imessage_);
+  }
+};
+
+TEST(Pipe, SimpleWriteReadWithAllTargetDevices) {
+  SimpleWriteReadWithAllTargetDevicesTest test;
+  test.run();
+}
+
+class SimpleWriteReadWithSomeTargetDevicesTest
+    : public ClientServerPipeTestCase {
+  InlineMessage imessage_ = {
+      .payloads =
+          {
+              {.data = "payload #1", .metadata = "payload metadata #1"},
+              {.data = "payload #2", .metadata = "payload metadata #2"},
+              {.data = "payload #3", .metadata = "payload metadata #3"},
+          },
+      .tensors =
+          {
+              {
+                  .data = "tensor #1",
+                  .metadata = "tensor metadata #1",
+                  .device = Device{kCpuDeviceType, 0},
+                  .targetDevice = Device{kCpuDeviceType, 0},
+              },
+              {
+                  .data = "tensor #2",
+                  .metadata = "tensor metadata #2",
+                  .device = Device{kCpuDeviceType, 0},
+              },
+              {
+                  .data = "tensor #3",
+                  .metadata = "tensor metadata #3",
+                  .device = Device{kCpuDeviceType, 0},
+                  .targetDevice = Device{kCpuDeviceType, 0},
+              },
+          },
+      .metadata = "pipe metadata",
+  };
+
+ public:
+  void server(Pipe& pipe) override {
+    Message message;
+    Storage storage;
+    std::tie(message, storage) = makeMessage(imessage_);
+    auto future = pipeWriteWithFuture(pipe, message);
+    future.get();
+  }
+
+  void client(Pipe& pipe) override {
+    Descriptor descriptor;
+    Storage storage;
+    auto future = pipeReadWithFuture(
+        pipe,
+        /*targetDevices=*/
+        {
+            Device{kCpuDeviceType, 0},
+            Device{kCpuDeviceType, 0},
+            Device{kCpuDeviceType, 0},
+        });
+    std::tie(descriptor, storage) = future.get();
+    expectDescriptorAndStorageMatchMessage(descriptor, storage, imessage_);
+  }
+};
+
+TEST(Pipe, SimpleWriteReadWithSomeTargetDevices) {
+  SimpleWriteReadWithSomeTargetDevicesTest test;
+  test.run();
+}
+
+class MultipleWriteReadTest : public ClientServerPipeTestCase {
+  InlineMessage imessage1_ = {
+      .payloads =
+          {
+              {.data = "payload #1.1", .metadata = "payload metadata #1.1"},
+          },
+      .tensors =
+          {
+              {
+                  .data = "tensor #1.1",
+                  .metadata = "tensor metadata #1.1",
+                  .device = Device{kCpuDeviceType, 0},
+              },
+          },
+      .metadata = "message metadata",
+  };
+
+  InlineMessage imessage2_ = {
+      .payloads =
+          {
+              {.data = "payload #2.1", .metadata = "payload metadata #2.1"},
+          },
+      .tensors =
+          {
+              {
+                  .data = "tensor #2.1",
+                  .metadata = "tensor metadata #2.1",
+                  .device = Device{kCpuDeviceType, 0},
+              },
+          },
+      .metadata = "message metadata",
+  };
+
+ public:
+  void server(Pipe& pipe) override {
+    Message message1;
+    Storage storage1;
+    std::tie(message1, storage1) = makeMessage(imessage1_);
+    auto future1 = pipeWriteWithFuture(pipe, message1);
+
+    Message message2;
+    Storage storage2;
+    std::tie(message2, storage2) = makeMessage(imessage2_);
+    auto future2 = pipeWriteWithFuture(pipe, message2);
+
+    future1.get();
+    future2.get();
+  }
+
+  void client(Pipe& pipe) override {
+    auto future1 = pipeReadWithFuture(
+        pipe,
+        /*targetDevices=*/
+        {
+            Device{kCpuDeviceType, 0},
+        });
+    auto future2 = pipeReadWithFuture(
+        pipe,
+        /*targetDevices=*/
+        {
+            Device{kCpuDeviceType, 0},
+        });
+
+    Descriptor descriptor1;
+    Storage storage1;
+    std::tie(descriptor1, storage1) = future1.get();
+    expectDescriptorAndStorageMatchMessage(descriptor1, storage1, imessage1_);
+
+    Descriptor descriptor2;
+    Storage storage2;
+    std::tie(descriptor2, storage2) = future2.get();
+    expectDescriptorAndStorageMatchMessage(descriptor2, storage2, imessage2_);
+  }
+};
+
+TEST(Pipe, MultipleWriteRead) {
+  MultipleWriteReadTest test;
+  test.run();
+}
+
+class MultipleWriteReadWithSomeTargetDevicesTest
+    : public ClientServerPipeTestCase {
+  InlineMessage imessage1_ = {
+      .payloads =
+          {
+              {.data = "payload #1.1", .metadata = "payload metadata #1.1"},
+          },
+      .tensors =
+          {
+              {
+                  .data = "tensor #1.1",
+                  .metadata = "tensor metadata #1.1",
+                  .device = Device{kCpuDeviceType, 0},
+              },
+          },
+      .metadata = "message metadata",
+  };
+
+  InlineMessage imessage2_ = {
+      .payloads =
+          {
+              {.data = "payload #2.1", .metadata = "payload metadata #2.1"},
+          },
+      .tensors =
+          {
+              {
+                  .data = "tensor #2.1",
+                  .metadata = "tensor metadata #2.1",
+                  .device = Device{kCpuDeviceType, 0},
+                  .targetDevice = Device{kCpuDeviceType, 0},
+              },
+          },
+      .metadata = "message metadata",
+  };
+
+ public:
+  void server(Pipe& pipe) override {
+    Message message1;
+    Storage storage1;
+    std::tie(message1, storage1) = makeMessage(imessage1_);
+    auto future1 = pipeWriteWithFuture(pipe, message1);
+
+    Message message2;
+    Storage storage2;
+    std::tie(message2, storage2) = makeMessage(imessage2_);
+    auto future2 = pipeWriteWithFuture(pipe, message2);
+
+    future1.get();
+    future2.get();
+  }
+
+  void client(Pipe& pipe) override {
+    auto future1 = pipeReadWithFuture(
+        pipe,
+        /*targetDevices=*/
+        {
+            Device{kCpuDeviceType, 0},
+        });
+    auto future2 = pipeReadWithFuture(
+        pipe,
+        /*targetDevices=*/
+        {
+            Device{kCpuDeviceType, 0},
+        });
+
+    Descriptor descriptor1;
+    Storage storage1;
+    std::tie(descriptor1, storage1) = future1.get();
+    expectDescriptorAndStorageMatchMessage(descriptor1, storage1, imessage1_);
+
+    Descriptor descriptor2;
+    Storage storage2;
+    std::tie(descriptor2, storage2) = future2.get();
+    expectDescriptorAndStorageMatchMessage(descriptor2, storage2, imessage2_);
+  }
+};
+
+TEST(Pipe, MultipleWriteReadWithSomeTargetDevices) {
+  MultipleWriteReadWithSomeTargetDevicesTest test;
+  test.run();
+}
---
+++ tensorpipe/test/core/pipe_test.h
@@ -0,0 +1,318 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <future>
+#include <memory>
+#include <tuple>
+#include <vector>
+
+#include <gtest/gtest.h>
+
+#include <tensorpipe/common/cpu_buffer.h>
+#include <tensorpipe/common/optional.h>
+#include <tensorpipe/tensorpipe.h>
+#include <tensorpipe/test/peer_group.h>
+
+struct Storage {
+  std::vector<std::shared_ptr<void>> payloads;
+  std::vector<std::pair<std::shared_ptr<void>, tensorpipe::Buffer>> tensors;
+};
+
+struct InlineMessage {
+  struct Payload {
+    std::string data;
+    std::string metadata;
+  };
+
+  struct Tensor {
+    std::string data;
+    std::string metadata;
+    tensorpipe::Device device;
+    tensorpipe::optional<tensorpipe::Device> targetDevice;
+  };
+
+  std::vector<Payload> payloads;
+  std::vector<Tensor> tensors;
+  std::string metadata;
+};
+
+inline std::pair<tensorpipe::Message, Storage> makeMessage(
+    InlineMessage imessage) {
+  tensorpipe::Message message;
+  Storage storage;
+
+  for (auto& payload : imessage.payloads) {
+    size_t length = payload.data.length();
+    auto data = std::unique_ptr<uint8_t, std::default_delete<uint8_t[]>>(
+        new uint8_t[length]);
+    std::memcpy(data.get(), &payload.data[0], length);
+    message.payloads.push_back({
+        .data = data.get(),
+        .length = length,
+        .metadata = payload.metadata,
+    });
+    storage.payloads.push_back(std::move(data));
+  }
+
+  for (auto& tensor : imessage.tensors) {
+    size_t length = tensor.data.length();
+    if (tensor.device.type == tensorpipe::kCpuDeviceType) {
+      auto data = std::unique_ptr<uint8_t, std::default_delete<uint8_t[]>>(
+          new uint8_t[length]);
+      std::memcpy(data.get(), &tensor.data[0], length);
+      tensorpipe::Buffer buffer = tensorpipe::CpuBuffer{.ptr = data.get()};
+      message.tensors.push_back({
+          .buffer = buffer,
+          .length = length,
+          .targetDevice = tensor.targetDevice,
+          .metadata = tensor.metadata,
+      });
+      storage.tensors.push_back({std::move(data), std::move(buffer)});
+    } else {
+      ADD_FAILURE() << "Unexpected source device: " << tensor.device.toString();
+    }
+  }
+
+  message.metadata = imessage.metadata;
+
+  return {std::move(message), std::move(storage)};
+}
+
+inline std::pair<tensorpipe::Allocation, Storage> makeAllocation(
+    const tensorpipe::Descriptor& descriptor,
+    const std::vector<tensorpipe::Device>& devices) {
+  tensorpipe::Allocation allocation;
+  Storage storage;
+  for (const auto& payload : descriptor.payloads) {
+    auto data = std::unique_ptr<uint8_t, std::default_delete<uint8_t[]>>(
+        new uint8_t[payload.length]);
+    allocation.payloads.push_back({.data = data.get()});
+    storage.payloads.push_back(std::move(data));
+  }
+
+  TP_DCHECK(devices.size() == descriptor.tensors.size());
+  for (size_t tensorIdx = 0; tensorIdx < descriptor.tensors.size();
+       ++tensorIdx) {
+    const auto& tensor = descriptor.tensors[tensorIdx];
+    tensorpipe::Device targetDevice = devices[tensorIdx];
+
+    if (tensor.targetDevice.has_value()) {
+      TP_DCHECK(targetDevice == *tensor.targetDevice);
+    }
+
+    if (targetDevice.type == tensorpipe::kCpuDeviceType) {
+      auto data = std::unique_ptr<uint8_t, std::default_delete<uint8_t[]>>(
+          new uint8_t[tensor.length]);
+      tensorpipe::Buffer buffer = tensorpipe::CpuBuffer{.ptr = data.get()};
+      allocation.tensors.push_back({.buffer = buffer});
+      storage.tensors.push_back({std::move(data), std::move(buffer)});
+    } else {
+      ADD_FAILURE() << "Unexpected target device: " << targetDevice.toString();
+    }
+  }
+
+  return {std::move(allocation), std::move(storage)};
+}
+
+inline std::future<void> pipeWriteWithFuture(
+    tensorpipe::Pipe& pipe,
+    tensorpipe::Message message) {
+  auto promise = std::make_shared<std::promise<void>>();
+  auto future = promise->get_future();
+
+  pipe.write(
+      std::move(message),
+      [promise{std::move(promise)}](const tensorpipe::Error& error) {
+        if (error) {
+          promise->set_exception(
+              std::make_exception_ptr(std::runtime_error(error.what())));
+          return;
+        }
+
+        promise->set_value();
+      });
+
+  return future;
+}
+
+inline std::future<std::tuple<tensorpipe::Descriptor, Storage>>
+pipeReadWithFuture(
+    tensorpipe::Pipe& pipe,
+    std::vector<tensorpipe::Device> targetDevices) {
+  auto promise = std::make_shared<
+      std::promise<std::tuple<tensorpipe::Descriptor, Storage>>>();
+  auto future = promise->get_future();
+  pipe.readDescriptor([&pipe,
+                       promise{std::move(promise)},
+                       targetDevices{std::move(targetDevices)}](
+                          const tensorpipe::Error& error,
+                          tensorpipe::Descriptor descriptor) mutable {
+    if (error) {
+      promise->set_exception(
+          std::make_exception_ptr(std::runtime_error(error.what())));
+      return;
+    }
+
+    tensorpipe::Allocation allocation;
+    Storage storage;
+    std::tie(allocation, storage) = makeAllocation(descriptor, targetDevices);
+    pipe.read(
+        std::move(allocation),
+        [promise{std::move(promise)},
+         descriptor{std::move(descriptor)},
+         storage{std::move(storage)}](const tensorpipe::Error& error) mutable {
+          if (error) {
+            promise->set_exception(
+                std::make_exception_ptr(std::runtime_error(error.what())));
+            return;
+          }
+
+          promise->set_value(std::make_tuple<tensorpipe::Descriptor, Storage>(
+              std::move(descriptor), std::move(storage)));
+        });
+  });
+
+  return future;
+}
+
+inline void expectDescriptorAndStorageMatchMessage(
+    tensorpipe::Descriptor descriptor,
+    Storage storage,
+    InlineMessage imessage) {
+  EXPECT_EQ(imessage.metadata, descriptor.metadata);
+
+  EXPECT_EQ(descriptor.payloads.size(), storage.payloads.size());
+  EXPECT_EQ(imessage.payloads.size(), storage.payloads.size());
+  for (size_t idx = 0; idx < imessage.payloads.size(); ++idx) {
+    EXPECT_EQ(
+        imessage.payloads[idx].metadata, descriptor.payloads[idx].metadata);
+    EXPECT_EQ(
+        imessage.payloads[idx].data.length(), descriptor.payloads[idx].length);
+    EXPECT_EQ(
+        imessage.payloads[idx].data,
+        std::string(
+            static_cast<char*>(storage.payloads[idx].get()),
+            descriptor.payloads[idx].length));
+  }
+
+  EXPECT_EQ(descriptor.tensors.size(), storage.tensors.size());
+  EXPECT_EQ(imessage.tensors.size(), storage.tensors.size());
+  for (size_t idx = 0; idx < imessage.tensors.size(); ++idx) {
+    EXPECT_TRUE(
+        imessage.tensors[idx].device == descriptor.tensors[idx].sourceDevice);
+    EXPECT_EQ(imessage.tensors[idx].metadata, descriptor.tensors[idx].metadata);
+    EXPECT_EQ(
+        imessage.tensors[idx].targetDevice,
+        descriptor.tensors[idx].targetDevice);
+    const tensorpipe::Device& device = storage.tensors[idx].second.device();
+    EXPECT_TRUE(
+        !imessage.tensors[idx].targetDevice ||
+        imessage.tensors[idx].targetDevice == device);
+    EXPECT_EQ(
+        imessage.tensors[idx].data.length(), descriptor.tensors[idx].length);
+    if (device.type == tensorpipe::kCpuDeviceType) {
+      const tensorpipe::CpuBuffer& buffer =
+          storage.tensors[idx].second.unwrap<tensorpipe::CpuBuffer>();
+      EXPECT_EQ(
+          imessage.tensors[idx].data,
+          std::string(
+              static_cast<char*>(buffer.ptr), descriptor.tensors[idx].length));
+    } else {
+      ADD_FAILURE() << "Unexpected target device: " << device.toString();
+    }
+  }
+}
+
+inline std::string createUniqueShmAddr() {
+  const ::testing::TestInfo* const testInfo =
+      ::testing::UnitTest::GetInstance()->current_test_info();
+  std::ostringstream ss;
+  // Once we upgrade googletest, also use test_info->test_suite_name() here.
+  ss << "shm://tensorpipe_test_" << testInfo->name() << "_" << getpid();
+  return ss.str();
+}
+
+inline std::vector<std::string> genUrls() {
+  std::vector<std::string> res;
+
+#if TENSORPIPE_HAS_SHM_TRANSPORT
+  res.push_back(createUniqueShmAddr());
+#endif // TENSORPIPE_HAS_SHM_TRANSPORT
+  res.push_back("uv://127.0.0.1");
+
+  return res;
+}
+
+inline std::shared_ptr<tensorpipe::Context> makeContext() {
+  auto context = std::make_shared<tensorpipe::Context>();
+
+  context->registerTransport(0, "uv", tensorpipe::transport::uv::create());
+#if TENSORPIPE_HAS_SHM_TRANSPORT
+  context->registerTransport(1, "shm", tensorpipe::transport::shm::create());
+#endif // TENSORPIPE_HAS_SHM_TRANSPORT
+  context->registerChannel(0, "basic", tensorpipe::channel::basic::create());
+#if TENSORPIPE_HAS_CMA_CHANNEL
+  context->registerChannel(1, "cma", tensorpipe::channel::cma::create());
+#endif // TENSORPIPE_HAS_CMA_CHANNEL
+
+  return context;
+}
+
+class ClientServerPipeTestCase {
+  ForkedThreadPeerGroup pg_;
+
+ public:
+  void run() {
+    pg_.spawn(
+        [&]() {
+          auto context = makeContext();
+
+          auto listener = context->listen(genUrls());
+          pg_.send(PeerGroup::kClient, listener->url("uv"));
+
+          std::promise<std::shared_ptr<tensorpipe::Pipe>> promise;
+          listener->accept([&](const tensorpipe::Error& error,
+                               std::shared_ptr<tensorpipe::Pipe> pipe) {
+            if (error) {
+              promise.set_exception(
+                  std::make_exception_ptr(std::runtime_error(error.what())));
+            } else {
+              promise.set_value(std::move(pipe));
+            }
+          });
+
+          std::shared_ptr<tensorpipe::Pipe> pipe = promise.get_future().get();
+          server(*pipe);
+
+          pg_.done(PeerGroup::kServer);
+          pg_.join(PeerGroup::kServer);
+
+          context->join();
+        },
+        [&]() {
+          auto context = makeContext();
+
+          auto url = pg_.recv(PeerGroup::kClient);
+          auto pipe = context->connect(url);
+
+          client(*pipe);
+
+          pg_.done(PeerGroup::kClient);
+          pg_.join(PeerGroup::kClient);
+
+          context->join();
+        });
+  }
+
+  virtual void client(tensorpipe::Pipe& pipe) = 0;
+  virtual void server(tensorpipe::Pipe& pipe) = 0;
+
+  virtual ~ClientServerPipeTestCase() = default;
+};
