42a67277c1882c90cec0da6e57afb20247424994
Luca Wehrstedt <lcw@fb.com>
2021-06-10 03:04:39 -0700

    Introduce tensorpipe_cuda.h
    Summary: So far this header does nothing. But we need it to exist so that we can migrate PyTorch. Once that's done, we can actually move stuff into this header. (All of this is because we cannot do atomic changes across both TensorPipe and PyTorch).
    
    Reviewed By: beauby
    
    Differential Revision: D28875275
    
    fbshipit-source-id: 99202950e93366adb83585acaf4317884dd1874c

---
+++ tensorpipe/tensorpipe_cuda.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) Facebook, Inc. and its affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+// For now we just provide this file so that we can migrate users over to it.
+// Once that's done, we can actually move some things from tensorpipe.h here.
+#include <tensorpipe/tensorpipe.h>
