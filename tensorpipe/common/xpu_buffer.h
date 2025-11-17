18b2ed5d5f6ab9ebdd826c0e3ce3d035b570f75f
Jeeja KP <jeeja.kp@intel.com>
2025-11-17 17:57:10 +0200

    Add XPU support in Tensorpipe.
    - added xpu_basic channel support.
    - added helper functions in common for xpu.
    - added test for xpu_basic channel
    
    Signed-off-by: Jeeja KP <jeeja.kp@intel.com>

---
+++ tensorpipe/common/xpu_buffer.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+#pragma once
+
+#include <sycl/sycl.hpp>
+#include <tensorpipe/common/device.h>
+
+namespace tensorpipe {
+
+struct XpuBuffer {
+  void* ptr{nullptr};
+  sycl::queue* queue{nullptr};
+
+  Device getDevice() const;
+};
+} // namespace tensorpipe
